b6dd176f93f4b4c85f2e0a2c64db83c31e8b2ff9
Natnaelkete <ketemanatnael023@gmail.com>
2024-11-15 12:29:36 -0800

    add controllers

---
+++ controllers/controller.js
@@ -0,0 +1,30 @@
+export const getName = (req, res) => {
+  try {
+    res.status(200).send("My name is Natnael Ketema");
+  } catch (error) {
+    console.log("From getName", error);
+  }
+};
+
+export const getHobby = (req, res) => {
+  try {
+    res.status(200).json({
+      1: "Coding",
+      2: "Taking courses",
+      3: "Playing football",
+    });
+  } catch (error) {
+    console.log("From getHobby", error);
+  }
+};
+export const getDream = (req, res) => {
+  try {
+    res
+      .status(200)
+      .send(
+        "I have a dream to be one of the great Ethiopian developer and programmer and promote my country to the world with tech related things"
+      );
+  } catch (error) {
+    console.log("From getDream", error);
+  }
+};
