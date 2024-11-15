c820ddb1bc3e9584fbfcdd7f0c7625b24e1fef29
Natnaelkete <ketemanatnael023@gmail.com>
2024-11-15 13:00:01 -0800

    add router inside server.js

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
+import InfoRoute from "./routes/route.js";
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/", InfoRoute);
 
 const PORT = 3000;
 
